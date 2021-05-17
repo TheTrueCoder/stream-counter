35c59bde938ee3bf998259e314984e0dca45e6b1
TheTrueCoder <38576090+TheTrueCoder@users.noreply.github.com>
2021-05-17 20:29:16 +1200

    The function webpage and api

--- api/main.py
+++ api/main.py
@@ -1,16 +1,28 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+vals_folder = "values"
 value_name = "kmsrun.txt"
+value_path = f"{vals_folder}/{value_name}"
+
+f = open(value_path, "w")
+f.close()
+del(f)
 
 def changeval(change_val):
-    with open(value_name, "r+") as f:
+    with open(value_path, "r+") as f:
         try:
             x = int(f.read())
         except:
             x = 0
         y = int(change_val)
         result = x+y
+
+        if result < 0:
+            result = 0
 
         f.seek(0)
         f.write(str(result))
---
+++ website/logic.js
@@ -0,0 +1,24 @@
+const urlParams = new URLSearchParams(window.location.search);
+const endpoint = urlParams.get('endpoint')
+console.log(endpoint)
+
+async function setCount(route) {
+    let resp = await fetch("http://" + endpoint + "/" + route);
+    console.log("http://" + endpoint + "/" + route)
+    if (resp.ok) { // if HTTP-status is 200-299
+        // get the resp body (the method explained below)
+        num = await resp.text();
+    } else {
+        alert("HTTP-Error: " + resp.status);
+    }
+
+    document.getElementById("distance").innerHTML = num + "km";
+}
+
+function addCount() {
+    setCount("increase");
+}
+
+function removeCount() {
+    setCount("decrease")
+}
