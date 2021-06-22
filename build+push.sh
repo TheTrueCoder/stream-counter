9023edd3212137b62952d5cd9c562d72d90ad45e
TheTrueCoder <38576090+TheTrueCoder@users.noreply.github.com>
2021-06-22 20:51:00 +1200

    Proxy works!

--- build+push.sh
+++ build+push.sh
@@ -1,6 +1,6 @@
 docker build -t aoteamaersk/counter-site .
-docker push aoteamaersk/counter-site
+# docker push aoteamaersk/counter-site
 
 cd api
 docker build -t aoteamaersk/counter-api .
-docker push aoteamaersk/counter-api
+# docker push aoteamaersk/counter-api
--- website/logic.js
+++ website/logic.js
@@ -3,7 +3,8 @@ const endpoint = urlParams.get('endpoint')
 console.log(endpoint)
 
 async function setCount(route) {
-    let resp = await fetch("http://" + endpoint + "/" + route);
+    // "http://" + endpoint + "/"
+    let resp = await fetch("/api/" + route);
     console.log("http://" + endpoint + "/" + route)
     if (resp.ok) { // if HTTP-status is 200-299
         // get the resp body (the method explained below)
