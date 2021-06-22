7a2e8f20275ac89546e38a26783c59506d9afd00
TheTrueCoder <38576090+TheTrueCoder@users.noreply.github.com>
2021-06-22 20:57:13 +1200

    Update logic.js

--- website/logic.js
+++ website/logic.js
@@ -1,11 +1,17 @@
 const urlParams = new URLSearchParams(window.location.search);
-const endpoint = urlParams.get('endpoint')
-console.log(endpoint)
+const endpoint = urlParams.get('endpoint');
+console.log(endpoint);
 
 async function setCount(route) {
-    // "http://" + endpoint + "/"
-    let resp = await fetch("/api/" + route);
-    console.log("http://" + endpoint + "/" + route)
+    if (endpoint == null) {
+        apiurl = "/api/";
+    }
+    else {
+        apiurl = "http://" + endpoint + "/";
+    }
+
+    let resp = await fetch(apiurl + route);
+    console.log(apiurl + route);
     if (resp.ok) { // if HTTP-status is 200-299
         // get the resp body (the method explained below)
         num = await resp.text();
@@ -21,5 +27,5 @@ function addCount() {
 }
 
 function removeCount() {
-    setCount("decrease")
+    setCount("decrease");
 }
